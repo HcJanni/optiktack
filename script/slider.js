012a8817c4b6c4531b9ea70fbf949833e7ff17c3
HcJanni <30379613+HcJanni@users.noreply.github.com>
2021-05-10 10:53:08 +0200

    Update slider.js

--- script/slider.js
+++ script/slider.js
@@ -10,32 +10,32 @@ function imgTransition(){
     video1.play();
     video1.style.opacity=1;
 }
-video1.onended = function(play2){
+video1.onended = function play2(){
     video2.play();
     video1.style.opacity=0;
     video2.style.opacity=1;
 }
-video2.onended = function(play3){
+video2.onended = function play3(){
     video3.play();
     video2.style.opacity=0;
     video3.style.opacity=1;
 }
-video3.onended = function(play4){
+video3.onended = function play4(){
     video4.play();
     video3.style.opacity=0;
     video4.style.opacity=1;
 }
-video4.onended = function(play5){
+video4.onended = function play5(){
     video5.play();
     video4.style.opacity=0;
     video5.style.opacity=1;
 }
-video5.onended = function(play6){
+video5.onended = function play6(){
     video6.play();
     video5.style.opacity=0;
     video6.style.opacity=1;
 }
-video6.onended = function(play1){
+video6.onended = function play1(){
     video1.play();
     video6.style.opacity=0;
     video1.style.opacity=1;
